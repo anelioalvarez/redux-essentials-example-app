4b63d0c79cfbe6b730f512b73f9066b14d37e3dc
anelioalvarez <anelio272014@gmail.com>
2021-02-11 16:18:57 -0300

    bug fixed: invalid user ID in UserPage component

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -10,8 +10,15 @@ const UserPage = ({ match }) => {
   const { userId } = match.params;
 
   const user = useSelector(state => selectUserById(state, userId));
-
   const postsForUser = useSelector(state => selectPostsByUser(state, userId));
+
+  if (!user) {
+    return (
+      <section>
+        <h2>User not found!</h2>
+      </section>
+    )
+  }
 
   const postTitles = postsForUser.map(post => (
     <li key={post.id}>
